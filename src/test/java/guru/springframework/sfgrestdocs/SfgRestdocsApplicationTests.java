56f00d72acd1807a8b81d147800d874bcc62253f
bbqboneless <sammylicea5@gmail.com>
2024-02-07 09:17:03 -0600

    Created Project

---
+++ src/test/java/guru/springframework/sfgrestdocs/SfgRestdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRestdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
